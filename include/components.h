6b204f1e3b4464a8645dd9a00bb50f4683fe1429
Stefano Piani <piani_stefano@hotmail.com>
2020-03-10 16:40:36 +0100

    Small improvements in the components.h file
    Added a function to retirieve the short_name (i.e. the symbol) of
    a component as a string. So, for example, for V this function
    returns the string "V".

--- include/components.h
+++ include/components.h
@@ -13,13 +13,16 @@ namespace Ddhdg
   };
 
   // This is useful for iterating over the previous enumerator
-  inline std::set<Component> all_components()
+  inline std::set<Component>
+  all_components()
   {
     std::set<Component> component_set{V, n, p};
     return component_set;
   }
 
-  inline std::string get_component_name(const Component c){
+  inline std::string
+  get_component_name(const Component c)
+  {
     switch (c)
       {
         case V:
@@ -29,10 +32,42 @@ namespace Ddhdg
         case p:
           return "hole_density";
         default:
-        Assert(false, UnknownComponent());
-        break;
+          Assert(false, UnknownComponent());
+          break;
       }
-      return "UnknownComponent";
+    return "UnknownComponent";
+  }
+
+  inline std::string
+  get_component_short_name(const Component c)
+  {
+    switch (c)
+      {
+        case V:
+          return "V";
+        case n:
+          return "n";
+        case p:
+          return "p";
+        default:
+          Assert(false, UnknownComponent());
+          break;
+      }
+    return "UnknownComponent";
+  }
+
+  inline unsigned int
+  get_component_number(const Component c)
+  {
+    unsigned int i = 0;
+    for (const Component current_cmp : all_components())
+      {
+        if (current_cmp == c)
+          return i;
+        i++;
+      }
+    Assert(false, UnknownComponent());
+    return 9999;
   }
 
   enum Displacement
@@ -42,7 +77,9 @@ namespace Ddhdg
     Wp
   };
 
-  inline std::string get_displacement_name(const Displacement d){
+  inline std::string
+  get_displacement_name(const Displacement d)
+  {
     switch (d)
       {
         case E:
@@ -52,8 +89,26 @@ namespace Ddhdg
         case Wp:
           return "hole_displacement";
         default:
-        Assert(false, UnknownDisplacement());
-        break;
+          Assert(false, UnknownDisplacement());
+          break;
+      }
+    return "UnknownDisplacement";
+  }
+
+  inline std::string
+  get_displacement_short_name(const Displacement d)
+  {
+    switch (d)
+      {
+        case E:
+          return "E";
+        case Wn:
+          return "Wn";
+        case Wp:
+          return "Wp";
+        default:
+          Assert(false, UnknownDisplacement());
+          break;
       }
     return "UnknownDisplacement";
   }
@@ -93,4 +148,17 @@ namespace Ddhdg
       }
     return V;
   }
+
+  inline std::string
+  get_displacement_name(const Component c)
+  {
+    return get_displacement_name(component2displacement(c));
+  }
+
+  inline std::string
+  get_displacement_short_name(const Component c)
+  {
+    return get_displacement_short_name(component2displacement(c));
+  }
+
 } // namespace Ddhdg
